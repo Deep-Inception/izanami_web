b95749d51d952dc51cb164a51bbda901f94a235a
SatoshiKonishi <s.k.september.24@gmail.com>
2020-04-06 00:57:01 +0900

    04/06 次はレコードの削除

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template,request
+from models.models import OnegaiContent
+from models.database import db_session
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -6,12 +9,31 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     name = request.args.get("name")
-    return render_template("index.html",name=name)
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html",name=name,all_onegai=all_onegai)
 
 @app.route("/index",methods=["post"])
 def post():
     name = request.form["name"]
-    return render_template("index.html",name=name)
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html",name=name,all_onegai=all_onegai)
+
+@app.route("/add",methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title,body,datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index()
+
+@app.route("/update",methods=["post"])
+def update():
+    content = OnegaiContent.query.filter_by(id=request.form["update"]).first()
+    content.title = request.form["title"]
+    content.body = request.form["body"]
+    db_session.commit()
+    return index()
 
 #おまじない
 if __name__ == "__main__":
