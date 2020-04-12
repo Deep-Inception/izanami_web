4ed0f7807c5342ee1f103c9adedb05f45b788996
yot0201412 <laughing.my.way0201@gmail.com>
2020-04-12 15:25:16 +0900

    repair migration

--- config/database.py
+++ config/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
+import sys
+sys.path.append('../')
 
 databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'onegai.db')
 engine = create_engine('sqlite:///' + databese_file, convert_unicode=True)
--- config/db_migrate.py
+++ config/db_migrate.py
@@ -1,2 +1,6 @@
-from models.database import init_db
+# python -m config.db_migrate
+
+import sys
+sys.path.append('../')
+from .database import init_db
 init_db()
