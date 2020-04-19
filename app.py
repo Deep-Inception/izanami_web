257d135d9200af14d9f351a0a377895b8bbfce42
SatoshiKonishi <s.k.september.24@gmail.com>
2020-04-19 15:46:43 +0900

    create login system

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-from flask import Flask,render_template,request
-from models.model import OnegaiContent
+from flask import Flask,render_template,request,session,redirect,url_for
+from models.model import OnegaiContent,User
 from config.database import db_session
 from datetime import datetime
+import key
+from hashlib import sha256
 
 app = Flask(__name__)
-
-from views.batch import batch_app
-app.register_blueprint(batch_app, url_prefix='/batch')
+app.secret_key = key.SECRET_KEY
 
 @app.route("/")
 @app.route("/index")
 def index():
-    name = request.args.get("name")
-    all_onegai = OnegaiContent.query.all()
-    return render_template("index.html",name=name,all_onegai=all_onegai)
-
-@app.route("/index",methods=["post"])
-def post():
-    name = request.form["name"]
-    all_onegai = OnegaiContent.query.all()
-    return render_template("index.html",name=name,all_onegai=all_onegai)
+    if "user_name" in session:
+        name = session["user_name"]
+        all_onegai = OnegaiContent.query.all()
+        return render_template("index.html",name=name,all_onegai=all_onegai)
+    else:
+        return redirect(url_for("top",status="logout"))
 
 @app.route("/add",methods=["post"])
 def add():
@@ -28,7 +25,7 @@ def add():
     content = OnegaiContent(title,body,datetime.now())
     db_session.add(content)
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
 
 @app.route("/update",methods=["post"])
 def update():
@@ -36,7 +33,7 @@ def update():
     content.title = request.form["title"]
     content.body = request.form["body"]
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
 
 @app.route("/delete",methods=["post"])
 def delete():
@@ -45,8 +42,53 @@ def delete():
         content = OnegaiContent.query.filter_by(id=id).first()
         db_session.delete(content)
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
 
-#おまじない
+@app.route("/top")
+def top():
+    status = request.args.get("status")
+    return render_template("top.html",status=status)
+
+@app.route("/login",methods=["post"])
+def login():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        password = request.form["password"]
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        if user.hashed_password == hashed_password:
+            session["user_name"] = user_name
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("top",status="wrong_password"))
+    else:
+        return redirect(url_for("top",status="user_notfound"))
+
+@app.route("/newcomer")
+def newcomer():
+    status = request.args.get("status")
+    return render_template("newcomer.html",status=status)
+
+@app.route("/registar",methods=["post"])
+def registar():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        return redirect(url_for("newcomer",status="exist_user"))
+    else:
+        password = request.form["password"]
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        user = User(user_name, hashed_password)
+        db_session.add(user)
+        db_session.commit()
+        session["user_name"] = user_name
+        return redirect(url_for("index"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user_name", None)
+    return redirect(url_for("top",status="logout"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
