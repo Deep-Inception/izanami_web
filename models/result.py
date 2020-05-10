47d5a890e739948a47db21814cd18773f93d5f46
yot0201412 <laughing.my.way0201@gmail.com>
2020-05-10 17:10:52 +0900

    add race_result_batch

---
+++ models/result.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Float, BOOLEAN, Enum
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import UniqueConstraint
+from config.database import Base
+from datetime import datetime
+import enum, re
+
+class Result(Base):
+    __tablename__ = "result"
+    id = Column(Integer, primary_key=True)
+    race_id = Column(Integer, ForeignKey("race.id"), nullable=False, unique=True)
+    win = Column(Integer, nullable=True, unique=False) # 単勝
+    exacta = Column(Integer, nullable=True, unique=False) # 二連単
+    quinella = Column(Integer, nullable=True, unique=False) # 二連複
+    trifecta = Column(Integer, nullable=True, unique=False) # 三連単
+    trio = Column(Integer, nullable=True, unique=False) # 三連複
+    show1 = Column(Integer, nullable=True, unique=False) # 複勝1位
+    show2 = Column(Integer, nullable=True, unique=False) # 複勝2位
+    created_at = Column(DateTime, unique=False, default=datetime.now())
+
+    def __init__(self):
+        self.win = None
+        self.exacta = None
+        self.quinella = None
+        self.trifecta = None
+        self.trio = None
+        self.show1 = None
+        self.show2 = None
+
+    def info(self):
+        return "win %s, exacta %s" % (self.win, self.exacta)
+
+    def set_params_from_dto(self, dto):
+        self.race_id = dto.race_id
+        self.win = dto.win
+        self.exacta = dto.exacta
+        self.quinella = dto.quinella
+        self.trifecta = dto.trifecta
+        self.trio = dto.trio
+        self.show1 = dto.show1
+        self.show2 = dto.show2
+        return self
