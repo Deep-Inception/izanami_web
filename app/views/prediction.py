eafe7b0342ea59c263f4f1636df75a005272d7dc
yot0201412 <laughing.my.way0201@gmail.com>
2020-07-05 14:41:23 +0900

    add test

--- app/views/prediction.py
+++ app/views/prediction.py
@@ -16,27 +16,43 @@ from machinelearning import ml_racer_pred_dl, preprocessing_racer_pred_dl
 predict_app = Blueprint("predict", __name__, template_folder="./templates", static_folder="./static")
 logger = logging.logging
 
-@predict_app.route("/test/")
+@predict_app.route("/fit/")
 def fix():
+    raw_data_df = train_data()
+    preprocessor = preprocessing_racer_pred_dl.RacerPredDlPreprocessor()
+    preprocessor.data_prepare(raw_data_df, 1)
+    X, y = preprocessor.get_prepared_data()
+    if not pd.isnull(y).any():
+        with open('machinelearning/racer_pred_dl_preprocessor.pickle', 'wb') as f:
+            pickle.dump(preprocessor, f)
+        ml_racer_pred_dl.fit(X, y)
+    return "ok"
+
+# モデルの学習に利用するデータを取得する　戻り値：dataframe
+def train_data():
     race_ids = [ r[0] for r in db_session.query(Race.id).filter(Race.status == RaceStatusEnum.FINISHED).all() ]
     race_df = Race.values_as_dataframe_by_ids(race_ids)
     timetable_racer_query = db_session.query(TimetableRacer.id).filter(TimetableRacer.race_id.in_(race_ids), TimetableRacer.exhibition_time != None).all()
     timetable_racer_ids = [ r[0] for r in timetable_racer_query ]
     timetable_racer_df = TimetableRacer.values_as_dataframe_by_ids(timetable_racer_ids)
     racer_result_df = RacerResult.values_as_dataframe_by_query(RacerResult.query.filter(RacerResult.timetable_racer_id.in_(timetable_racer_ids)))
+    merged_df = merge_train_data(race_df, timetable_racer_df, racer_result_df)
+    raw_data_df = raw_data(merged_df)
+    return raw_data_df
+
+# モデル学習用のデータをJOINする
+def merge_train_data(race_df, timetable_racer_df, racer_result_df):
     merged_df = pd.merge(race_df, timetable_racer_df)
     merged_df = pd.merge(merged_df, racer_result_df)
     merged_df.sort_values(["race_id", "prize"], inplace=True)
+    return merged_df
+
+# 学習に必要な行、列のみを取得し、適宜カラム名、変更する
+def raw_data(merged_df):
     raw_data_df = merged_df[["time", "place", "deadline", "distance", "couse", "racer_id", "exhibition_time"]]
-    raw_data_df.dropna(how='any', inplace=True)
+    raw_data_df.dropna(how='any',subset=["place", "deadline", "distance", "couse", "racer_id", "exhibition_time"], inplace=True)
     raw_data_df.rename(columns={"time": "RACE_TIME", "place": "PLACE","deadline": "RACE_DATE", "distance": "DISTANCE", "couse": "COUSE", "racer_id": "RACER_ID", "exhibition_time": "EXHIBITION_TIME"}, inplace=True)
-    preprocessor = preprocessing_racer_pred_dl.RacerPredDlPreprocessor()
-    preprocessor.data_prepare(raw_data_df, 1)
-    X, y = preprocessor.get_prepared_data()
-    with open('machinelearning/racer_pred_dl_preprocessor.pickle', 'wb') as f:
-        pickle.dump(preprocessor, f)
-    ml_racer_pred_dl.fit(X, y)
-    return "ok"
+    return raw_data_df
 
 @predict_app.route("/predict/")
 def predict():
@@ -64,6 +80,7 @@ def predict():
     y_pred = ml_racer_pred_dl.predict(X)
     for ttr_id , time in zip(list(merged_df["timetable_racer_id"]), y_pred):
         result = RacerPredictionDL(timetable_racer_id=ttr_id, time=time[0], version=ml_racer_pred_dl.VERSION)
+        logger.DEBUG("timetable_racer_id: %i, version: %i" % (result.ttr_id, ml_racer_pred_dl.VERSION))
         db_session.add(result)
         db_session.commit()
         db_session.expunge(result)
---
+++ tests/test_machinelearning.py
@@ -0,0 +1,26 @@
+import pytest
+import pandas as pd
+from app.machinelearning import preprocessing_racer_pred_dl
+
+def test_fill_null_time_001():
+    pp = preprocessing_racer_pred_dl.RacerPredDlPreprocessor()
+    pp.y = pd.DataFrame(data=[[1], [None], [None], [7]], columns=["RACE_TIME"])
+    pp.fill_null_time()
+    pp.y[0] = 1
+    pp.y[1] = 2
+    pp.y[2] = 3
+    pp.y[3] = 7
+    assert len(pp.y) == 4;
+
+def merged_data_for_test():
+    return pd.DataFrame(data=[[1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, None, 7], [1, 2, 3, 4, 5, 6, 7]],
+                        columns=["time", "place", "deadline", "distance", "couse", "racer_id", "exhibition_time"])
+
+def race_data_for_test():
+    return pd.DataFrame(data=[[1], [2]], columns=["race_id"])
+
+def racer_data_for_test():
+    return pd.DataFrame(data=[[1, 1],[1, 2], [2, 3]], columns=["race_id", "timetable_racer_id"])
+
+def race_result_data_for_test():
+    return pd.DataFrame(data=[[1, 1],[2, 2], [3, 1]], columns=["timetable_racer_id", "prize"])
