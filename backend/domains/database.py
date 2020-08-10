06ca73e86eae3f2e14ddab66814cf970aa14807e
SatoshiKonishi <s.k.september.24@gmail.com>
2020-08-10 16:55:36 +0900

    db_migrateとlogの修正

--- backend/domains/database.py
+++ backend/domains/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../database/izanami.db')
+databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../initialdatasetup/izanami.db')
 engine = create_engine('sqlite:///' + databese_file, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base = declarative_base()
