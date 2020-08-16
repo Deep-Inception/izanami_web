de8103aa03fba76261a62a1609d1d566854ac95d
SatoshiKonishi <s.k.september.24@gmail.com>
2020-08-16 16:10:29 +0900

    refactoring

--- frontend/src/components/top.vue
+++ frontend/src/components/top.vue
@@ -54,29 +54,9 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+
 </script>
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
