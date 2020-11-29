f725ce9cf36dbb44f6939be4adf8329a66003c29
yot0201412 <laughing.my.way0201@gmail.com>
2020-11-29 15:47:21 +0900

    パラメータがなかったら今日

--- backend/controllers/race.py
+++ backend/controllers/race.py
@@ -11,7 +11,8 @@ logger = logging.logging
 @race_bp.route('/')
 def index():
     date_str = request.args.get('date')
-    date_str = date_str if date_str != None else "20200815"
+    today = datetime.datetime.today()
+    date_str = date_str if date_str != None else today.strftime("%Y%m%d%")
     date_sta = datetime.datetime.strptime( date_str + "0000", "%Y%m%d%H%M")
     date_en = datetime.datetime.strptime( date_str + "2359", "%Y%m%d%H%M")
     races = Race.query.filter(Race.deadline >= date_sta, Race.deadline <= date_en).all()
