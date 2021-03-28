b5775f9e2c1dc4070144e149ca79d397c1f20ea6
yot0201412 <laughing.my.way0201@gmail.com>
2021-03-28 14:28:46 +0900

    開発モードと本番モードを引数で引数で変更できるように変更

--- backend/configs/config.py
+++ backend/configs/config.py
@@ -6,3 +6,13 @@ class BaseConfig(object):
     SECRET_KEY = os.urandom(24)
     # csrf token用秘密鍵
     WTF_CSRF_SECRET_KEY = os.urandom(24)
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8&auth_plugin_map={auth_plugin_map}'.format(**{
+      'user': 'root',
+      'password': 'izaname_web',
+      'host': 'izanami-mysql',
+      'db_name': 'db_izanami',
+      'auth_plugin_map' : {'mysql_native_password'}
+    })
