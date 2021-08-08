1f9f669ef1acacc36b81911c5531f80e702ad25b
Satoshi Konishi <satoshikonishi@SatoshinoMacBook-Air.local>
2021-08-08 17:00:48 +0900

    vue front 修正

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -2,16 +2,19 @@
   <header class="text-gray-600 body-font bg-blue-500">
     <div class="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
       <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-        <a class="mr-5 text-white hover:text-gray-200">Home</a>
+        <a href="/index" class="mr-5 text-white hover:text-gray-200">Home</a>
       </nav>
-      <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+      <div class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
         <span class="ml-3 text-xl text-white">Izanami</span>
-      </a>
+      </div>
       <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Something</button>
+        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          <p v-if=loginScreen>Sing Up</p>
+          <p v-else>Sing Out</p>
+        </button>
       </div>
     </div>
   </header>
@@ -19,6 +22,7 @@
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  props: ['loginScreen']
 }
 </script>
