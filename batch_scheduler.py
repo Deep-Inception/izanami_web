f5b121f582d40996b38631824ddc5444f0753e20
yot0201412 <laughing.my.way0201@gmail.com>
2021-08-09 17:23:35 +0900

    batchにpredict追加 バッチエラー修正

--- batch_scheduler.py
+++ batch_scheduler.py
@@ -1,5 +1,5 @@
 import schedule
-import time, date
+import time, datetime
 from backend.controllers import batch, prediction
 
 def race_index_job():
@@ -7,19 +7,20 @@ def race_index_job():
 
 def before_info_job():
     batch.before_info_batch()
+    prediction.predict()
 
 def race_result_job():
     batch.race_result_batch()
 
 def prediction_fix_job():
-    if date.today().day != 1:
+    if datetime.date.today().day != 1:
         return
     prediction.fix()
 
 schedule.every().day.at("04:00").do(prediction_fix_job)
 schedule.every().day.at("02:00").do(race_index_job)
 schedule.every(5).minutes.do(before_info_job)
-schedule.every(5).minutes.do(race_result_job)
+schedule.every(30).minutes.do(race_result_job)
 
 while True:
     schedule.run_pending()
