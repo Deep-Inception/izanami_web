bdb0aa44b00d713c208fe9c2e5d3a06d92bb93ba
yot0201412 <laughing.my.way0201@gmail.com>
2021-08-14 00:31:20 +0900

    Getに失敗した場合に、5回までリトライする

--- backend/models/scraper/before_info.py
+++ backend/models/scraper/before_info.py
@@ -1,9 +1,19 @@
 from bs4 import BeautifulSoup
 from .items.before_info_racer import BeforeInfoRacer
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
 def get_data(url):
-    r = requests.get(url)
+    session = requests.Session()
+    retries = Retry(total=5,  # リトライ回数
+                backoff_factor=1,  # sleep時間
+                status_forcelist=[500, 502, 503, 504])  # timeout以外でリトライするステータスコード
+    session.mount("https://", HTTPAdapter(max_retries=retries))
+    session.mount("http://", HTTPAdapter(max_retries=retries))
+    r = session.get(url=url,
+                       stream=True,
+                       timeout=(10.0, 20.0))
     re = BeforeInfo(r)
     re.scrape()
     return re
